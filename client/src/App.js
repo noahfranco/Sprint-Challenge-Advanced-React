35e704308617d882543c726a609a5111528976c0
noahfranco <noahfra@gmail.com>
2019-09-06 16:11:06 -0400

    created Nabbar componenet and passed down darkmode hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import './App.css';
 import axios from "axios"; 
-import Styled from "styled-components"; 
 import PlayerCard from './components/PlayerCard';
-import UserForm from "./components/UserForm.js"; 
-
-const Margin = Styled.div`
-margin: 20px; 
-`
 
 class App extends React.Component { 
 constructor() {
@@ -40,9 +34,6 @@ render() {
   return (
     <div className="App">
      <h1> Women's World Cup </h1> 
-     <h3> Register for more information </h3>
-     <UserForm /> 
-     <Margin> </Margin>
       { this.state.players.map(player => (
        <PlayerCard player={player} key={player.id} /> 
      ))}
